54e8c32aec07c9a50a0d5521c6d911600e92de25
Mahi Ajay <mahiajay16@gmail.com>
2023-07-22 10:58:40 +0530

    basic info

--- AI_ML/day1.py
+++ AI_ML/day1.py
@@ -1,1 +1,17 @@
-#.
+"""#bracket "<>-NA  []-indexing/subscripting/slicing,list  {}-dict, set  ()-functions arg,expressions, tuple"
+#identifiers
+-it must contain alpha numeri only
+-special char not allowed except underscore
+-Cna not start from number
+#Operators
+1. arithmetic: +,-,*,/,%(modulo),**(power), //(floor div)
+2. assignment: =,+=,-+,...............
+3. relational: >,<, >=,<=,==,!=
+4. logical: and, or, not
+5. Bitwise: &(and),|(or),^(ex-or),~(not),>>(right shift),<<(left shift) ( #m>>n =>m /2^n      #m<< n => m*2^n)
+6. membership: in, not in
+7. Identity: is,is not
+#Data & types
+# Build-in Functions for data types
+# control statement
+# """
